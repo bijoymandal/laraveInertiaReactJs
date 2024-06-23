a0190366090089379a76daa5f6a6c3bcf4a47463
bijoymandal <bijoymandal00@gmail.com>
2024-06-23 07:45:45 +0530

    product model add fieds

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+    ];
 }
